fc1b2f2a77b00621231324c65a59e2ba16f3791d
sheoak <dev@sheoak.fr>
2015-01-27 11:10:26 +0100

    fix(install): Fix install script
    Multi-line was broken
    Add zathurarc backup

--- install-system.sh
+++ install-system.sh
@@ -5,46 +5,53 @@
 #
 # Distributed under terms of the MIT license.
 
-apt-get update
+sudo apt-get update
 
-sudo apt-get install -y
-    \ urxvt-unicode
-    \ nodejs
-    \ npm
-    \ git
-    \ subversion
-    \ build-essential
-    \ ubuntu-restricted
-    \ ubuntu-restricted-extras
-    \ htop
-    \ console-data
-    \ chromium-browser
-    \ filezilla
-    \ ubuntu-restricted-extras
-    \ ubuntu-restricted-addons
-    \ build-essential
-    \ gimp
-    \ unrar
-    \ htop
-    \ thunar
-    \ unzip
-    \ thunar-archive-plugin
-    \ vim-common
-    \ nodejs
-    \ ruby-full
+sudoapt-get install -y \
+     urxvt-unicode \
+     nodejs \
+     npm \
+     git \
+     subversion \
+     build-essential \
+     ubuntu-restricted \
+     ubuntu-restricted-extras \
+     htop \
+     console-data \
+     chromium-browser \
+     filezilla \
+     ubuntu-restricted-extras \
+     ubuntu-restricted-addons \
+     build-essential \
+     gimp \
+     unrar \
+     htop \
+     thunar \
+     unzip \
+     thunar-archive-plugin \
+     vim-common \
+     nodejs \
+     ruby-full
 
 # ranger
-sudo apt-get install -y ranger caca-utils highlight atool w3m poppler-utils mediainfo
+sudo apt-get install -y \
+    ranger \
+    caca-utils \
+    highlight \
+    atool \
+    w3m \
+    poppler-utils \
+    mediainfo
 
 # LAMP
-sudo apt-get install -y
-    \ apache2
-    \ php5
-    \ mysql-server
-    \ libapache2-mod-php5
-    \ php5-mysql
-    \ phpmyadmin
-    \ php5-curl
+sudo apt-get install -y \
+     apache2 \
+     php5 \
+     mysql-server \
+     libapache2-mod-php5 \
+     php5-mysql \
+     phpmyadmin \
+     php5-curl \
 
 sudo gem install sass
 sudo gem install compass
@@ -54,5 +61,6 @@ sudo npm install -g pleeease jshint sails
 a2enmod headers rewrite
 
 # FIXME zathura bug if not in etc?
+sudo cp /etc/zathurarc /etc/zathurarc.bak
 sudo cp .zathura/zathurarc /etc/zathurarc
 
