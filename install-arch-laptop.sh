f16c1213242f447384db18affad9a1458f04131e
sheoak <gomunochikin@gmail.com>
2018-01-20 23:40:29 +0100

    Clean install scripts

--- install-arch-laptop.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-#
-# install-arch-laptop.sh
-# Copyright (C) 2015 Sheoak <dev@sheoak.fr>
-#
-# Distributed under terms of the MIT license.
-#
-
-sudo pacman -S xf86-input-synaptics xorg-xinput
--- install-common.sh
+++ install-common.sh
@@ -15,18 +15,20 @@ DOTFILES="`pwd`"
 ln -s $DOTFILES/.Xdefaults $HOME/
 ln -s $DOTFILES/.zshrc $HOME/
 ln -s $DOTFILES/.config/ranger $HOME/.config/
-ln -s $DOTFILES/.zathura/zathurarc $HOME/.config/zathura
+ln -s $DOTFILES/.zathura/ $HOME/
 ln -s $DOTFILES/.xcolors $HOME/
 ln -s $DOTFILES/.ncmpcpp $HOME/
 ln -s $DOTFILES/.conkyrc $HOME/
 ln -s $DOTFILES/.vimrc $HOME/
 ln -s $DOTFILES/.vimrc.bepo.keymap $HOME/
 ln -s $DOTFILES/.vimrc.typematrix.keyboard $HOME/
-ln -s $DOTFILES/.vimperatorrc $HOME/
+#deprecated, i keep it here for people who still use it…
+#ln -s $DOTFILES/.vimperatorrc $HOME/
 ln -s $DOTFILES/.vimiumrc $HOME/
 
 if [ ! -d $HOME/.oh-my-zsh ];
 then
+    # warning, check repo first!
     git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
     chsh -s /bin/zsh
 fi
