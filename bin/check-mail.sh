f95bb7ab6425291f9a95cfbadfe7de036a007034
Sheoak <sheoak@users.noreply.github.com>
2019-08-25 17:34:26 +0200

    Update mail checking script

--- bin/check-mail.sh
+++ bin/check-mail.sh
@@ -1,11 +1,19 @@
 #! /bin/bash
 shopt -s nullglob
 WHERE=(~/.mail/*/Inbox/new/*)
-
 COUNT=${#WHERE[@]}
+CHECK_SERVICE=`systemctl is-active --user mbsync.service`
+OUT=""
+COLOR_ERR="%{F#dc322f}%{F-}"
 
 if [[ $COUNT -gt 0 ]]; then
-    echo " $COUNT"
+    OUT=" $COUNT"
 else
-    echo ""
+    OUT=""
 fi
+
+if [[ $CHECK_SERVICE = 'failed' ]]; then
+    [[ -z $OUT ]] && OUT=" %{F#dc322f}mbsync error%{F-}" || OUT="$OUT %{F#dc322f}[mbsync error]%{F-}"
+fi
+
+echo $OUT
