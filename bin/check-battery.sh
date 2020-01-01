3a1195cf8baaf95f0acfe425413c1fc77a364034
sheoak <sheoak@users.noreply.github.com>
2020-01-01 11:45:12 +0100

    fix: Update battery script for thinkpad

--- bin/check-battery.sh
+++ bin/check-battery.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-BATTINFO=`acpi -b`
-if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:15:00 ]] ; then
+BATTINFO=`acpi -b | grep -v unavailable | head -n 1`
+if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 4 -d " " | cut -f 1 -d '%'` < 20 ]] ; then
     DISPLAY=:0.0 /usr/bin/notify-send -u critical "    Low battery!" "$BATTINFO"
     /usr/bin/paplay /usr/share/sounds/freedesktop/stereo/suspend-error.oga
 fi
