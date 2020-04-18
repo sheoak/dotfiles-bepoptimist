95dbfc70116bf10cb78d936c7d00c236653facbe
sheoak <sheoak@users.noreply.github.com>
2020-04-18 13:52:13 +0200

    fix: Fix kittyfzf script window

--- bin/kittyfzf.sh
+++ bin/kittyfzf.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 xdotool search --onlyvisible --classname KittyFuzzy windowunmap \
   || xdotool search --classname KittyFuzzy windowmap \
-  || kitty --name KittyFuzzy --detach -e sh -c 'kitty --single-instance -e -c sh xdg-open $(fzf --color=bw)'
+  || kitty --name KittyFuzzy --detach -e sh -c 'kitty --detach -e -c sh nohup xdg-open $(fzf --color=bw) > /dev/null 2>&1'
